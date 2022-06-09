149715a91c28a697f30d2f063c7e2f63cfb1ad80
Magnum Cortez <magnum.cortez@hotmail.com>
2022-06-09 11:15:16 -0300

    test: validate emails with domain larger than 255 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -12,7 +12,22 @@ describe('Email validation', () => {
   })
 
   test('shoud accept valid email', () => {
-    const email = 'any@mail.com'
+    const email = 'local@domain.com'
     expect(Email.validate(email)).toBeTruthy()
+  })
+
+  test('shoud not accept strings larger than 320 chars', () => {
+    const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('shoud not accept domain part larger than 64 chars', () => {
+    const email = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('shoud not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(66) + '@domain.com'
+    expect(Email.validate(email)).toBeFalsy()
   })
 })
