2385ddae5c3d210391db00f6bdedcfebd609dded
Magnum Cortez <magnum.cortez@hotmail.com>
2022-06-09 11:27:53 -0300

    feat: validate additional invalid emails

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -4,16 +4,26 @@ export class Email {
       return false
     }
 
+    const emailRegex = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+    if (!emailRegex.test(email)) {
+      return false
+    }
+
     if (email.length > 320) {
       return false
     }
 
     const [local, domain] = email.split('@')
-    if (local.length > 64) {
+    if (local.length > 64 || local.length === 0) {
       return false
     }
 
-    if (domain.length > 255) {
+    if (domain.length > 255 || domain.length === 0) {
+      return false
+    }
+
+    const domainParts = domain.split('.')
+    if (domainParts.some(part => part.length > 63)) {
       return false
     }
 
