dc4ef70f54db2f16f07c6d8007788ab155403aa5
Magnum Cortez <magnum.cortez@hotmail.com>
2022-06-14 20:31:27 -0300

    feat: config register route

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,5 +1,5 @@
 import { Express } from 'express'
-import { bodyParser, cors, contentType } from '@/main/middleware'
+import { bodyParser, cors, contentType } from '@/main/config/middleware/index'
 
 export default (app: Express): void => {
   app.use(bodyParser)
