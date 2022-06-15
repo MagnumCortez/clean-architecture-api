68e20fa5c996b9340dc20f7881f4c4a6c5bb2d70
Magnum Cortez <magnum.cortez@hotmail.com>
2022-06-15 18:42:25 -0300

    chore: configure jest mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  preset: '@shelf/jest-mongodb',
   roots: [
     '<rootDir>/test'
   ],
@@ -8,7 +9,6 @@ module.exports = {
     '!**/test/**',
     '!**/config/**'
   ],
-  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
